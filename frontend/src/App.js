8c7c9ca20af9c233543496c6592753dc5ef3df46
Megan Echano <mdechano@up.edu.ph>
2024-03-04 15:49:21 +0800

    setup route to dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter , Route , Routes} from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage'
+import Dashboard from './components/pages/Dashboard'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <div>
           <Routes>
             <Route exact={true} path="/" element={<LandingPage />} />
+            <Route exact={true} path="/dashboard" element={<Dashboard />} />
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -1,8 +1,15 @@
+import {Link} from 'react-router-dom'
+import '../css/Dashboard.css'
 
 function Dashboard () {
+
+    //functions here
+
     return (
         <div>
             BOOM DASHBOARD
         </div>
     )
 }
+
+export default Dashboard;

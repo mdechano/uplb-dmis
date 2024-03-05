cbaafe46c2f7f55ec61e5c5daf67fc894728b442
Megan Echano <mdechano@up.edu.ph>
2024-03-05 15:38:07 +0800

    dashboard nav bar

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -1,5 +1,7 @@
 import {Link} from 'react-router-dom'
 import '../css/Dashboard.css'
+import UPLBLogoText from '../images/UPLBLogoText.png'
+import DDMenu from '../images/DDMenu.png'
 
 function Dashboard () {
 
@@ -7,8 +9,27 @@ function Dashboard () {
 
     return (
         <div>
-            <header>
-                BOOM DASHBOARD
+            <header className='dashboard_header'>
+                <div className='left-header'>
+                    <div class="dropdown">
+                        <button class="dropbtn">MENU</button>
+                        <div class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <img className='uplblogo' src={UPLBLogoText} alt='logo' />
+                </div>
+                
+                <nav>
+                    <ul className="dashboard-nav_links">
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Contact</a></li>
+                    </ul>
+                </nav>
+                <button className='logoutbtn'>Logout</button>
             </header>
         </div>
     )

dfe1f1fffcb47e5c5e37865e545a1f46447384c4
Megan Echano <mdechano@up.edu.ph>
2024-03-08 17:00:03 +0800

    SISPersonal updates

--- frontend/src/components/pages/StudentInfoSheetPersonal.js
+++ frontend/src/components/pages/StudentInfoSheetPersonal.js
@@ -9,7 +9,37 @@ function StudentInfoSheetPersonal () {
             <NavBar></NavBar>
 
             <div className='stud-info-sheet-div'>
-                Student Info Sheet Personal.
+                <div className='upper-div'>
+                    <button className='back-button'>BACK</button>
+                    <p className='page-title'>STUDENT INFORMATION SHEET</p>
+                    <button className='save-button'>SAVE</button>
+                </div>
+                <div className='body-div'>
+                    <div className='left-div'>
+                        <div className='student-div'>
+                            <div className='image-div'>
+                                image here
+                            </div>
+                            <div className='profile-info'>
+                                <p>ANNA DELA CRUZ</p>
+                                <p>2019-08206</p>
+                                <p>ROOM NO. 1209</p>
+                            </div>
+                        </div>
+                        <div className='nav-div'>
+                            <button className='stud-info-sheet-nav-personal'>PERSONAL</button>
+                            <button className='stud-info-sheet-nav'>FAMILY</button>
+                            <button className='stud-info-sheet-nav'>CHECK IN</button>
+                            <button className='stud-info-sheet-nav'>EMERGENCY</button>
+                            <button className='stud-info-sheet-nav'>PAYMENT</button>
+                            <button className='stud-info-sheet-nav'>VIOLATION</button>
+                        </div>
+                    </div>
+                    <div className='right-div'>
+                        ISANG MALAKING FORM
+                    </div>
+                </div>
+                
             </div>
         </div>
     )

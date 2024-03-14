b806e27bb43c85f228fb31e5feb756f594f63f2f
Megan Echano <mdechano@up.edu.ph>
2024-03-14 22:08:06 +0800

    login function attempts

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -1,24 +1,74 @@
-import {Link} from 'react-router-dom'
+import {Link, Redirect, Navigate} from 'react-router-dom'
 import '../css/Dashboard.css'
 import NavBar from '../pages/NavBar';
+import Cookies from "universal-cookie";
+import { Component } from 'react';
 
-function Dashboard () {
+export default class Dashboard extends Component {
 
     //functions here
 
-    return (
-        <div>
-            <NavBar></NavBar>
+    constructor(props) {
+        super(props);
 
-            <div className='dashboard-notice'>
-                <p className='greet'><b>Greetings, resident!</b></p>
-                <br></br>
-                <br></br>
-                <p>Welcome to  UPLB DMIS.  We are pleased to welcome you to ___ Resident Hall. You are required to fill up or update the student  information sheet as mandated on or before your check in date. You may also generate your statements of account in the SOA section. Payment receipts must be uploaded in the Receipt section. Please see said sections under your profile icon.<br></br><br></br>All information are subject to viewing and checking of the dorm manager and dorm assistants.</p>
-            </div>
+        this.state = {
+            checkedIfLoggedIn: false,
+            isLoggedIn: null,
+            email: localStorage.getItem("email")
+        }
+    }
 
-        </div>
-    )
+    componentDidMount() {
+        // send POST request to check if user is logged in
+
+        fetch(
+            "http://localhost:3001/checkifloggedin",
+            {
+                method: "POST",
+                credentials: "include"
+            }
+        )
+        .then(response => response.json())
+        .then(body => {
+            if (body.isLoggedIn) {
+                this.setState({checkedIfLoggedIn: true, isLoggedIn: true, email: localStorage.getItem("email")})
+            } else {
+                this.setState({checkedIfLoggedIn: true, isLoggedIn: false});
+            }
+        });
+    }
+
+
+    render () {
+        if (!this.state.checkedIfLoggedIn) {
+            return(
+                <div></div>
+            )
+        } else {
+            if (this.state.isLoggedIn) {
+                //render the page
+                return (
+                    <div>
+                        <NavBar></NavBar>
+
+                        <div className='dashboard-notice'>
+                            <p className='greet'><b>Greetings, resident!</b></p>
+                            <br></br>
+                            <br></br>
+                            <p>Welcome to  UPLB DMIS.  We are pleased to welcome you to ___ Resident Hall. You are required to fill up or update the student  information sheet as mandated on or before your check in date. You may also generate your statements of account in the SOA section. Payment receipts must be uploaded in the Receipt section. Please see said sections under your profile icon.<br></br><br></br>All information are subject to viewing and checking of the dorm manager and dorm assistants.</p>
+                        </div>
+
+                    </div>
+                )
+            }
+            else {
+                // redirect
+                return <Navigate to="/" />
+            }
+        }
+        
+    }
+    
 }
 
-export default Dashboard;
+// export default Dashboard;
--- frontend/src/components/pages/LoginPage.js
+++ frontend/src/components/pages/LoginPage.js
@@ -1,38 +1,117 @@
-import {Link} from 'react-router-dom'
+import {Link, Routes, Router, useNavigate} from 'react-router-dom';
 import '../css/LoginPage.css'
+import React, {Component} from "react";
+import Cookies from "universal-cookie";
+import { Navigate, Route} from 'react-router';
 
-function LoginPage () {
+export default class LoginPage extends Component  {
 
-    return (
-        // <div className='form-div'>
-            <form action="action_page.php" method="post">
-                <div class="imgcontainer">
-                    {/* <img src="img_avatar2.png" alt="Avatar" class="avatar"/> */}
-                </div>
-                <div class='login-text'>
-                    Login to UPLB DMIS
-                </div>
+    constructor(props) {
+        super(props);
 
-                <div class="container-1">
-                    <label for="uname"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="uname" required />
+        // this.state = {
+        //     success: false
+        // };
 
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required />
+        this.login = this.login.bind(this);
+    }
 
-                    <button type="submit" class='loginbtn'><a><Link to='/dashboard'>Login</Link></a></button>
-                    <label>
-                    <input type="checkbox" checked="checked" name="remember"/> Remember me
-                    </label>
-                </div>
+    login(e) {
+        e.preventDefault();
 
-                <div class="container-2">
-                    <span class="psw"><a><Link to='/signup'>Don't have an account?</Link></a></span>
-                </div>
-            </form>
-        // </div>
-    )
+        const credentials = {
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        }
+
+        // const navigate = useNavigate();
+
+        // send a POST request
+        fetch(
+            "http://localhost:3001/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(credentials)
+            }
+        )
+        .then(response => response.json())
+        .then(body => {
+            if (!body.success) {
+                alert("Failed to log in!");
+            }
+            else {
+                // successful log in, store token as cookie
+            
+                const cookies = new Cookies();
+                cookies.set(
+                    "authToken",
+                    body.token,
+                    {
+                        path: "localhost:3001/",
+                        age: 60*60,
+                        sameSite: "lax"
+                    }
+                );
+
+                localStorage.setItem("email", body.email);
+                alert("Successfully logged in!");
+                // navigate('/dashboard');
+                // this.setState({success:true});
+
+                
+                
+                
+            }
+        })
+
+        // if(this.setState({success:true})) {
+        //     <Navigate to='/dashboard' />
+        // }
+    }
+
+
+
+    render() {
+        return (
+            <div className='form-div'>
+                <form action="action_page.php" method="post">
+                    <div class="imgcontainer">
+                        {/* <img src="img_avatar2.png" alt="Avatar" class="avatar"/> */}
+                    </div>
+                    <div class='login-text'>
+                        Login to UPLB DMIS
+                    </div>
+
+                    <div class="container-1">
+                        <label for="email"><b>Email</b></label>
+                        <input type="text" placeholder="Enter Email" name="email" id="email" required />
+
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" id="password" required />
+
+                        {/* <button type="submit" class='loginbtn'><a><Link to='/dashboard'>Login</Link></a></button> */}
+                        <button id="login" onClick={this.login}>Login</button>
+                        <label>
+                        <input type="checkbox" checked="checked" name="remember"/> Remember me
+                        </label>
+                    </div>
+
+                    <div class="container-2">
+                        <span class="psw"><a><Link to='/signup'>Don't have an account?</Link></a></span>
+                    </div>
+                </form>
+
+            
+            </div>
+
+            
+
+        )
+    }
 
 }
 
-export default LoginPage;
+// export default LoginPage;

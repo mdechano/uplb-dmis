8ebdd3551fa28257c806eb00a3c38de9e05fcede
Megan Echano <mdechano@up.edu.ph>
2024-03-18 21:46:37 +0800

    update dashboard.js

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -32,8 +32,45 @@ const Dashboard = () => {
                 
                 <br></br>
                 <br></br>
-                <p>Welcome to  UPLB DMIS.  We are pleased to welcome you to ___ Resident Hall. You are required to fill up or update the student  information sheet as mandated on or before your check in date. You may also generate your statements of account in the SOA section. Payment receipts must be uploaded in the Receipt section. Please see said sections under your profile icon.<br></br><br></br>All information are subject to viewing and checking of the dorm manager and dorm assistants.</p>
+
+                { role === 'student' ?
+                    <form className='complete-sign-up'>
+                        <h3>Let's get you set up</h3>
+                        <p>Please choose a role.</p>
+                        <div className='custom-select'>
+                        <select className='user-role' id='user-role'>
+                            <option>Choose Role</option>
+                            <option value='dorm manager'>Dorm Manager</option>
+                            <option value='resident'>Resident</option>
+                        </select>   
+                        </div>
+                        
+                        <br></br>
+                        <p>Choose your assigned dormitory.</p>
+                        <div class="custom-select">
+                        <select id="resident_hall">
+                            <option value="Women's Residence Hall">Women's Residence Hall</option>
+                            <option value="Men's Residence Hall">Men's Residence Hall</option>
+                            <option value="International House Residence Hall">International House Residence Hall</option>
+                            <option value="VetMed Residence Hall">VetMed Residence Hall</option>
+                            <option value="Makiling Residence Hall">Makiling Residence Hall</option>
+                            <option value="ATI-NTC Residence Hall">ATI-NTC Residence Hall</option>
+                            <option value="Forestry Residence Hall">Forestry Residence Hall</option>
+                            <option value="New Forestry Residence Hall">New Forestry Residence Hall</option>
+                            <option value="New Dormitory Residence Hall">New Dormitory Residence Hall</option>
+                        </select>
+                        </div>
+
+                        <br></br>
+                        <button className='complete-sign-up'>Complete Sign Up</button>
+                    </form>
+                    :
+                    ""
+                }
+                
             </div>
+
+            {/* <p>Welcome to  UPLB DMIS.  We are pleased to welcome you to ___ Resident Hall. You are required to fill up or update the student  information sheet as mandated on or before your check in date. You may also generate your statements of account in the SOA section. Payment receipts must be uploaded in the Receipt section. Please see said sections under your profile icon.<br></br><br></br>All information are subject to viewing and checking of the dorm manager and dorm assistants.</p> */}
 
         </div>
     )

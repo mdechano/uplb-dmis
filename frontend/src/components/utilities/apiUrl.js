8b3308c0d17222825a89a79a854545ffdcd6500b
Megan Echano <mdechano@up.edu.ph>
2024-05-27 13:34:23 +0800

    update apiUrl.js

--- frontend/src/components/utilities/apiUrl.js
+++ frontend/src/components/utilities/apiUrl.js
@@ -1,4 +1,5 @@
-const url = "http://localhost:3001"
+// const url = "http://localhost:3001"
+const url = "https://uplb-dormitory-management-information.onrender.com"
 export const apiUrl = (endpoint) => {
   return url + endpoint;
 };
